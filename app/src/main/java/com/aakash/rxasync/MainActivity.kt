822a38f7f45110ee1dbca8cd26855b956efec1c0
akashmehta <akashmehta4444@gmail.com>
2019-07-26 18:15:35 +0530

    Added count down timer using rx.

--- app/src/main/java/com/aakash/rxasync/MainActivity.kt
+++ app/src/main/java/com/aakash/rxasync/MainActivity.kt
@@ -3,9 +3,9 @@ package com.aakash.rxasync
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import io.reactivex.Observable
-import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,12 +16,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setTimer()
+    }
+
+    override fun onDestroy() {
+        compositeDisposable.dispose()
+        super.onDestroy()
+    }
+
+    private fun setTimer() {
+        rxTimer(1000, 5000, onRepeat = {
+            tvText.text = "$it"
+        }, onComplete = {
+            tvText2.text = "Complete"
+        }).addTo(compositeDisposable)
+    }
+
+    private fun setDelayedOperation() {
         val disposable = Observable.just(1)
             .setDelayedCallback(2000, {
                 tvText.text = "Hi I am Aakash"
             })
         compositeDisposable.add(disposable)
+    }
 
+    private fun setAsyncOperation() {
         val observable1 = Observable.fromCallable {
             return@fromCallable ""
         }
@@ -30,11 +49,6 @@ class MainActivity : AppCompatActivity() {
                 tvText.text = "Hi I am Aakash 1"
             }
         compositeDisposable.add(observable1)
-    }
-
-    override fun onDestroy() {
-        compositeDisposable.dispose()
-        super.onDestroy()
     }
 
 }
--- app/src/main/java/com/aakash/rxasync/UiObservable.kt
+++ app/src/main/java/com/aakash/rxasync/UiObservable.kt
@@ -3,6 +3,7 @@ package com.aakash.rxasync
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 
 fun <T> Observable<T>.uiObservable() : Observable<T> = this.subscribeOn(AndroidSchedulers.mainThread())
@@ -13,3 +14,15 @@ fun <T> Observable<T>.setDelayedCallback(
     onSubscribe: () -> Unit, timeUnit: TimeUnit = TimeUnit.MILLISECONDS): Disposable = this.delay(time, timeUnit)
     .uiObservable()
     .subscribe { onSubscribe() }
+
+
+fun rxTimer(timeInterval: Long, duration: Long, onRepeat: (time: Long) -> Unit = {}, onComplete: () -> Unit = {}): Disposable =
+    Observable.interval(0, timeInterval, TimeUnit.MILLISECONDS)
+        .take(duration, TimeUnit.MILLISECONDS)
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .doOnNext {
+            onRepeat(it)
+        }.doOnComplete {
+            onComplete()
+        }.subscribe()
